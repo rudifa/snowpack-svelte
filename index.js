19da92a5122baef9fb6a511a69d3bab6bd4c269b
Rudi Farkas <rudi.farkas@gmail.com>
2021-10-10 17:19:36 +0200

    add src/App.svelte, call from index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 /* Add JavaScript code here! */
 console.log('Hello World! You did it! Welcome to Snowpack :D');
+
+import App from './src/App.svelte';
+
+const app = new App({
+    target: document.body,
+});
+
+export default app;
---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<style>
+    h1 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        margin: 0;
+        animation: wave 1.5s infinite;
+    }
+
+    h2 {
+        text-align: center;
+        color: #111;
+        position: absolute;
+        top: 55%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    a {
+        color: #FF3E00;
+    }
+
+    @keyframes wave {
+        0%   { transform: rotate(0) translate(-50%, -50%); }
+        10%  { transform: rotate(-5deg) translate(-50%, -50%); }
+        20%  { transform: rotate(10deg) translate(-50%, -50%); }
+        30%  { transform: rotate(-5deg) translate(-50%, -50%); }
+        40%  { transform: rotate(10deg) translate(-50%, -50%); }
+        50%  { transform: rotate(0) translate(-50%, -50%); }
+        100% { transform: rotate(0) translate(-50%, -50%); }
+    }
+</style>
+
+<h1>👋</h1>
+<h2>
+    Welcome to <a href="https://svelte.dev/">Svelte</a>,<br />
+    bundled by <a href="https://www.snowpack.dev/">Snowpack</a>
+</h2>
